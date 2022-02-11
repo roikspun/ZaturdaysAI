6346c0b8918e998d16d0baa9b3e2c1debf5ecf7e
Erick Espindola <73044601+roikspun@users.noreply.github.com>
2022-02-11 18:38:15 +0100

    Update Cardio_3_Streamlit.py

--- Cardio_3_Streamlit.py
+++ Cardio_3_Streamlit.py
@@ -1,1 +1,11 @@
-print('hello world')
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import streamlit as st
+import matplotlib as plt
+
+cardio = pd.read_csv('cardio_train.csv', sep=';')
+cardio.shape 
+
+st.write("My first Streamlit App")
+st.dataframe(cardio
